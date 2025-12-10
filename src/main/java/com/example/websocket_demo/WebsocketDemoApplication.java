1100f13d4da457e3d01eeedbae793a9f563a0294
mono <baihao885521@gmail.com>
2025-12-10 23:38:13 +0800

    Add files via upload

---
+++ src/main/java/com/example/websocket_demo/WebsocketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocket_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(WebsocketDemoApplication.class, args);
+	}
+
+}
